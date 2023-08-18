240edbb134b7dc1ed8544f3f0ee5bf2bbe8bbd2f
gaojianyu97 <3294381311@qq.com>
2023-08-18 18:19:19 +0800

    完成计时器链表的维护模块

---
+++ src/timer/lst_timer.cpp
@@ -0,0 +1,221 @@
+#include "lst_timer.h"
+
+sort_timer_lst::sort_timer_lst(){
+    head = NULL;
+    tail = NULL;
+}
+
+sort_timer_lst::~sort_timer_lst(){
+    util_timer *tmp = head;
+    while(tmp){
+        head = tmp->next;
+        delete tmp;
+        tmp = head;
+    }
+}
+
+void sort_timer_lst::add_timer(util_timer *timer,util_timer *lst_head){
+    util_timer *prev = lst_head;//链表头
+    util_timer *tmp = prev->next;
+
+    //按照过期时间从小到大的顺序将计时器插入双向链表
+    while(tmp){
+        if(timer->expire<tmp->expire){
+            prev->next = timer;
+            timer->next = tmp;
+            tmp->prev = timer;
+            timer->prev = prev;
+            break;
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+    //链表仅有一个头，且新进入计时器过期时间大于头节点（参考add_timer(util_timer *timer)）
+    if(!tmp){
+        prev->next = timer;
+        timer->next = NULL;
+        timer->prev = prev;
+        tail = timer;
+    }
+}
+
+void sort_timer_lst::add_timer(util_timer *timer){
+    //timer为空代表不添加
+    if(!timer)
+        return;
+    
+    //链表为空，新加入计时器作为头尾
+    if(!head){
+        head = tail = timer;
+        return;
+    }
+
+    //计时器过期时间小于头部，将计时器作为链表的头
+    if(timer->expire<head->expire){
+        timer->next = head;
+        head->prev = timer;
+        head = timer;
+        return;
+    }
+    add_timer(timer, head);
+}
+
+void sort_timer_lst::adjust_timer(util_timer *timer){
+    // 如果 timer 为 NULL，直接返回，不进行任何操作
+    if(!timer)
+        return;
+
+    util_timer *tmp = timer->next;
+    // 如果 timer 的下一个计时器为 NULL 或者 timer 的过期时间小于下一个计时器的过期时间，直接返回
+    if(!tmp||timer->expire<tmp->expire)
+        return;
+    
+    // 如果 timer 是链表头部的计时器
+    if(timer==head){
+        head = head->next;// 更新头指针，将头指针指向 timer 的下一个计时器
+        head->prev = NULL;// 更新新的头指针的前驱为 NULL
+        timer->next = NULL;// 将 timer 的 next 指针置为 NULL，因为它将成为新的链表的最后一个节点
+        add_timer(timer, head);// 将 timer 插入到新的链表中
+    }else{
+         // 如果 timer 不是链表头部的计时器
+        timer->prev->next = timer->next;
+        timer->next->prev = timer->prev;
+        add_timer(timer, timer->next);
+    }
+}
+
+void sort_timer_lst::del_timer(util_timer *timer){
+    //要删除的计时器为空
+    if(!timer)
+        return;
+    //要删除的计时器为头和尾
+    if(timer==head&&timer==tail){
+        delete timer;
+        head = NULL;
+        tail = NULL;
+        return;
+    }
+
+    //要删除的计时器为头
+    if(timer==head){
+        head = head->next;
+        head->prev = NULL;
+        delete timer;
+        return;
+    }
+
+    //要删除的计时器为尾
+    if(timer==tail){
+        tail = tail->prev;
+        tail->next = NULL;
+        delete timer;
+        return;
+    }
+
+    //正常删除
+    timer->prev->next = timer->next;
+    timer->next->prev = timer->prev;
+    delete timer;
+    return;
+}
+
+void sort_timer_lst::tick(){
+    if(!head)
+        return;
+
+    time_t cur = time(NULL);
+    util_timer *tmp = head;
+    //循环对头计时器进行判定
+    while(tmp){
+        if(cur<tmp->expire)break;
+
+        tmp->cb_func(tmp->user_data);
+        head = tmp->next;
+        if(head)
+            head->prev = NULL;
+        delete tmp;
+        tmp = head;
+    }
+}
+
+void utils::init(int timeslot){
+    m_timeslot = timeslot;//触发事件时间初始化
+}
+
+int utils::set_nonblocking(int fd){
+    
+    int old_option = fcntl(fd, F_GETFL);// 获取文件描述符的旧的文件状态标志
+    int new_option = old_option | O_NONBLOCK;// 将旧的文件状态标志与 O_NONBLOCK 进行按位或操作，设置为非阻塞模式
+    fcntl(fd, F_SETFL, new_option);// 将新的文件状态标志设置为文件描述符的状态
+    return old_option;// 返回旧的文件状态标志，以便在需要时进行恢复
+}
+
+void utils::add_fd(int epoll_fd,int fd,bool one_shot,int trig_mode){
+    epoll_event event;
+    event.data.fd = fd;
+
+    // 根据传入的触发模式设置事件类型
+    if(trig_mode==1){
+        event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;//监听可读事件（有数据可读），使用边缘触发模式，并且可以检测连接的对端关闭
+    }else{
+        event.events = EPOLLIN | EPOLLRDHUP;//监听可读事件（有数据可读），并且可以检测连接的对端关闭
+    }
+
+    // 如果需要单次触发，则添加 EPOLLONESHOT 标志（事件只触发一次后即从epoll示例的就绪队列移除）
+    if(one_shot){
+        event.events |= EPOLLONESHOT;
+    }
+
+    // 将事件添加到 epoll 监听中
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event);
+
+    // 设置文件描述符为非阻塞模式
+    set_nonblocking(fd);
+}
+
+void utils::sig_handler(int sig){
+    int save_error = errno;// 保存当前的 errno 值
+    int msg = sig;// 将收到的信号值存储到 msg 变量中
+    send(u_pipefd[1], (char *)&msg, 1, 0);// 向管道的写端发送信号值
+    errno = save_error;// 向管道的写端发送信号值
+}
+
+void utils::add_sig(int sig,void(handler)(int),bool restart){
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = handler;// 设置信号处理函数
+
+    // 如果需要重启系统调用，则设置 SA_RESTART 标志
+    if(restart){
+        sa.sa_flags |= SA_RESTART;
+    }
+    // 如果需要重启系统调用，则设置 SA_RESTART 标志
+    sigfillset(&sa.sa_mask);
+
+    // 使用 sigaction 函数设置信号处理动作
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+void utils::timer_handler(){
+    m_timer_lst.tick();// 触发定时器链表中已到期的计时器的回调函数
+    alarm(m_timeslot);// 重新设置定时器，以便下一次触发
+}
+
+void utils::show_error(int conn_fd,const char *info){
+    send(conn_fd, info, strlen(info), 0);// 发送错误信息到连接套接字
+    close(conn_fd);// 关闭连接套接字
+}
+
+int *utils::u_pipefd = 0;
+int utils::u_epollfd = 0;
+
+class utils;
+
+// 回调函数，用于处理超时的客户连接
+void cb_func(client_data *user_data){
+    epoll_ctl(utils::u_epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
+    assert(user_data);
+    close(user_data->sockfd);
+    http_conn::m_user_count--;
+}
---
+++ src/timer/lst_timer.h
@@ -0,0 +1,173 @@
+#ifndef LST_TIMER_H
+#define LST_TIMER_H
+
+#include "./log/log.h"
+#include "./httpconn/http_conn.h"
+
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <string.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
+
+class util_timer;
+
+/**
+ * @brief 客户端数据结构
+*/
+struct client_data{
+    sockaddr_in address; //客户端地址
+    int sockfd;          //客户端套接字描述符
+    util_timer *timer;   //计时器指针
+};
+
+
+/**
+ * @brief util_timer工具类，管理计时器相关信息和操作
+*/
+class util_timer{
+public:
+    util_timer():prev(NULL),next(NULL) {}
+
+public:
+    time_t expire;                  //计时器过期时间
+    void (*cb_func)(client_data *); //计时器回调函数指针
+    client_data *user_data;         //用户数据
+    util_timer *prev;               //前一个计时器
+    util_timer *next;               //后一个计时器
+};
+
+
+/**
+ * @brief sort_timer_lst类排序的计时器链表，用于管理多个计时器对象，并按照计时器的过期时间进行排序和触发
+*/
+class sort_timer_lst{
+private:
+    util_timer *head;//计时器链表头部
+    util_timer *tail;//计时器链表尾部
+
+    /**
+     * @brief 将计时器添加到链表中
+     * @param timer 计时器指针
+     * @param lst_head 计时器链表头
+    */
+    void add_timer(util_timer *timer, util_timer *lst_head);
+
+public:
+    /**
+     * @brief 构造函数，计时器链表初始化
+    */
+    sort_timer_lst();
+
+    /**
+     * @brief 析构函数，计时器链表资源回收
+    */
+    ~sort_timer_lst();
+    
+    /**
+     * @brief 添加计时器
+    */
+    void add_timer(util_timer *timer);
+
+    /**
+     * @brief 调整计时器
+    */
+    void adjust_timer(util_timer *timer);
+
+    /**
+     * @brief 删除计时器
+    */
+    void del_timer(util_timer *timer);
+
+    /**
+     * @brief 计时器触发处理
+    */
+    void tick();
+};
+
+/**
+ * @brief utils工具箱类，epoll任务处理
+*/
+class utils{
+public:
+    static int *u_pipefd;       //管道描述符
+    sort_timer_lst m_timer_lst; //排序计时器链表
+    static int u_epollfd;       //epoll实例描述符
+    int m_timeslot;             //最小超时单位
+
+public:
+    /**
+     * @brief 构造函数，工具类初始化
+    */
+    utils();
+
+    /**
+     * @brief 析构函数，离开作用域时进行资源回收
+    */
+    ~utils();
+
+    /**
+     * @brief 初始化时间槽大小
+     * @param timeslot 时间槽大小
+    */
+    void init(int timeslot);
+
+    /**
+     * @brief 设置文件描述符为非阻塞模式
+     * @param fd 文件描述符
+    */
+    int set_nonblocking(int fd);
+
+    /**
+     * @brief 注册文件描述符到 epoll 事件表
+     * @param epollfd epoll文件描述符
+     * @param fd 要注册的文件描述符
+     * @param one_shot 是否开启EPOLLONESHOT模式
+     * @param trig_mode 触发模式
+    */
+    void add_fd(int epollfd, int fd, bool one_shot, int trig_mode);
+
+    /**
+     * @brief 信号处理
+     * @param sig 信号
+    */
+    static void sig_handler(int sig);
+
+    /**
+     * @brief 设置信号处理函数
+     * @param sig 要注册的信号编号
+     * @param handler 信号处理函数的指针
+     * @param restart 是否启用信号重启机制
+    */
+    void add_sig(int sig, void(handler)(int), bool restart = true);
+
+    /**
+     * @brief 定时处理任务
+    */
+    void timer_handler();
+
+    /**
+     * @brief 向指定的连接发送错误信息，并关闭连接
+     * @param conn_fd 连接文件描述符
+     * @param info 要发送的错信息字符串
+    */
+    void show_error(int conn_fd, const char *info);
+};
+
+/**
+ * @brief 回调函数，关闭超时的客户端连接，并清理相应的资源
+*/
+void cb_func(client_data *user_data);
+
+#endif LST_TIMER_H
